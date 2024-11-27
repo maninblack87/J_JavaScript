32119b23c49be086d676f9e2b24f9b4f4d498fb3
maninblack87 <maninblack87@naver.com>
2024-11-28 05:07:46 +0900

    241128 1st

--- study/nodejs01/app.js
+++ study/nodejs01/app.js
@@ -2,15 +2,15 @@ const express = require(`express`);
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send(`Hello Express3~!`);
-});
+    res.send(`Hello Express!`);
+})
 
-// 바디파서 미들웨어 등록
-app.use(express.json());
+// 라우트 미들웨어 등록
+app.use(express.json);
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/contacts", require(`./routes/route_midware`));
 
 app.listen(3000, () => {
-    console.log("서버 실행중~!");
-});
+    console.log("nodejs01에서 서버 실행중..");
+})
--- study/nodejs01/routes/route_midware.js
+++ study/nodejs01/routes/route_midware.js
@@ -1,4 +1,4 @@
-const express = require(`express`);
+const express = require("express");
 const router = express.Router();
 
 router
@@ -10,22 +10,21 @@ router
         console.log(req.body);
         const { name, email, phone } = req.body;
         if (!name || !email || !phone) {
-            return res.send("필수 값이 입력되지 않았습니다.");
+            return res.send(`필수 값이 입력되지 않았습니다.`);
         }
-
         res.send(`Create Contact`);
-    });
+    })
 
 router
     .route("/:id")
     .get((req, res) => {
-        res.send(`View Contact for ID : ${req.params.id}`);
+        res.send(`View contact for ID : ${req.params.id}`);
     })
     .put((req, res) => {
-        res.send(`Update Contact for ID : ${req.params.id}`);
+        res.send(`Update contact for ID : ${req.params.id}`);
     })
     .delete((req, res) => {
-        res.send(`Delete Contact for ID : ${req.params.id}`);
+        res.send(`Delete contact for ID : ${req.params.id}`);
     })
 
 // router 내보내기
